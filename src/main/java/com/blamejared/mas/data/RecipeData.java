02d12f8521dcb6e3b2726303cde907a7916089da
Jared <jaredlll08@gmail.com>
2017-05-10 08:51:40 +0200

    Added a recipe for the Crank

--- src/main/java/com/blamejared/mas/data/RecipeData.java
+++ src/main/java/com/blamejared/mas/data/RecipeData.java
@@ -4,14 +4,15 @@ import com.blamejared.mas.blocks.MBlocks;
 import net.minecraft.init.*;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.*;
 
 public class RecipeData {
     
     public static void init() {
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.GENERATOR_TRASH), "ifi", "fsf", "ifi", 'f', new ItemStack(Blocks.FURNACE), 's', "cobblestone", 'i', "ingotIron"));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.GENERATOR_COAL), "ifi", "fcf", "ifi", 'f', new ItemStack(Blocks.FURNACE), 'c', new ItemStack(Items.COAL), 'i', "ingotIron"));
-        
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.CRANK), " ww", " w ", "sss", 'w', "planksWood", 's', "stone"));
+    
     }
     
 }
