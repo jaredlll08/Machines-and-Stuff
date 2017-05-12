f14e94702ebc97c39dea3c655b998e8a14d21fa1
Jared <jaredlll08@gmail.com>
2017-05-12 03:18:59 +0200

    Changed the recipes to use Reinforced Stone

--- src/main/java/com/blamejared/mas/data/RecipeData.java
+++ src/main/java/com/blamejared/mas/data/RecipeData.java
@@ -9,9 +9,9 @@ import net.minecraftforge.oredict.*;
 public class RecipeData {
     
     public static void init() {
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.GENERATOR_TRASH), "ifi", "fsf", "ifi", 'f', new ItemStack(Blocks.FURNACE), 's', "cobblestone", 'i', "ingotIron"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.GENERATOR_TRASH), "ifi", "fsf", "ifi", 'f', new ItemStack(Blocks.FURNACE), 's', new ItemStack(MBlocks.STONE_REINFORCED), 'i', "ingotIron"));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.GENERATOR_COAL), "ifi", "fcf", "ifi", 'f', new ItemStack(Blocks.FURNACE), 'c', new ItemStack(Items.COAL), 'i', "ingotIron"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.CRANK), " ww", "twt", "sss", 'w', "plankWood", 's', "stone", 't', new ItemStack(Blocks.STONE_SLAB)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.CRANK), " ww", "twt", "sss", 'w', "plankWood", 's', new ItemStack(MBlocks.STONE_REINFORCED), 't', new ItemStack(Blocks.STONE_SLAB)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MBlocks.STONE_REINFORCED), "sis", "isi", "sis", 'i', "ingotIron", 's', "stone"));
     }
     
